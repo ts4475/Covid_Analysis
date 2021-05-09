b4143f0a0b2d9170db59ea3851463a1a9c405f2c
Tiyasha Samanta <83566090+ts4475@users.noreply.github.com>
2021-05-09 19:39:57 +0530

    Update Queries_exec.sql

--- Queries_exec.sql
+++ Queries_exec.sql
@@ -51,6 +51,29 @@ WHERE death_and_recovery.Patient_status='Recovered';
 SELECT COUNT(Patient_status)
 FROM death_and_recovery
 WHERE Patient_status="Recovered" And State="Assam";
+                                
+
+DECLARE 
+   age number(10) := 0;
+   age1 number(10) := 24; 
+ 
+BEGIN 
+   age:= 20;
+   age1:= 25;
+  
+
+  -- check the boolean condition using if statement  
+   IF( age < 25) THEN 
+      -- if condition is true then print the following   
+      dbms_output.put_line('Age is between 20-24' ); 
+   ELSE
+      dbms_output.put_line('Age is above 24' ); 
+
+   END IF; 
+   dbms_output.put_line('Total Population affected within 20-24 is 35%') ; 
+END; 
+/
+
 
 
 SELECT State_UT,Hospitals_Available,Beds_Available
